e26ac4da7984f76c00228a111ed627e98993f154
Jochen Lillich <jochen@lillich.info>
2017-11-27 17:11:20 +0000

    Fix zsh script for rbenv
    **Why is this change necessary?**
    
    **How does it address the issue?**
    
    **What side effects does this change have?**
    
    **References**

--- zsh/rbenv.zsh
+++ zsh/rbenv.zsh
@@ -1,5 +1,5 @@
 # load rbenv if available
 RBENV_PATH="$HOME/.rbenv/bin"
-test -d RBENV_PATH && export PATH="${RBENV_PATH}:${PATH}"
+test -d $RBENV_PATH && export PATH="${RBENV_PATH}:${PATH}"
 RBENV=$(which rbenv)
 test -x "$RBENV" && eval "$($RBENV init -)"
