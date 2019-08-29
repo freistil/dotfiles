2c49d1a5a01b60161f45f5cee6791447f9c3fb08
Jochen Lillich <jochen@lillich.info>
2019-08-29 14:54:37 +0100

    Fix user/host in zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -6,4 +6,4 @@ git_prompt_info() {
   fi
 }
 setopt promptsubst
-export PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) %# '
+export PS1='%{$fg_bold[green]%}%n@%m:%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) %# '
